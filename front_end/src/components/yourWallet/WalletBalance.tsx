d808f9bb6d192b601da25824b1cc54e15ded0f42
tianroy <madwoo@gmail.com>
2022-01-01 04:03:04 +0800

    can read contract

--- front_end/src/components/yourWallet/WalletBalance.tsx
+++ front_end/src/components/yourWallet/WalletBalance.tsx
@@ -3,14 +3,24 @@ import { useEthers, useTokenBalance } from "@usedapp/core"
 import { formatUnits } from "@ethersproject/units"
 import { BalanceMsg } from "../../components/BalanceMsg"
 
+import { constants, utils } from "ethers"
+import TokenFarm from "../../chain-info/contracts/TokenFarm.json"
+import networkMapping from "../../chain-info/deployments/map.json"
+
 export interface WalletBalanceProps {
     token: Token
 }
 
 export const WalletBalance = ({ token }: WalletBalanceProps) => {
+    const { chainId } = useEthers()
+    const { abi } = TokenFarm
+    const tokenFarmAddress = chainId ? networkMapping[String(chainId)]["TokenFarm"][0] : constants.AddressZero
+
+
+
     const { image, address, name } = token
     const { account } = useEthers()
-    const tokenBalance = useTokenBalance(address, account)
+    const tokenBalance = useTokenBalance(address, tokenFarmAddress)
     const formattedTokenBalance: number = tokenBalance ? parseFloat(formatUnits(tokenBalance, 18)) : 0
     return (<BalanceMsg
         label={`Your un-staked ${name} balance`}
