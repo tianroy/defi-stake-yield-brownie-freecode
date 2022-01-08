a2afe9cdbf36d2f77732b1091c3f8ba579718746
tianroy <madwoo@gmail.com>
2022-01-08 21:31:14 +0800

    front end improve
    Co-authored-by: Henry Li

---
+++ front_end/src/hooks/useSellBid.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+import { useEthers, useContractFunction } from "@usedapp/core"
+import { constants, utils } from "ethers"
+import TokenFarm from "../chain-info/contracts/TokenFarm.json"
+import { Contract } from "@ethersproject/contracts"
+import networkMapping from "../chain-info/deployments/map.json"
+
+export const useSellBid = (amount: number) => {
+
+    const { chainId } = useEthers()
+    const { abi } = TokenFarm
+    const tokenFarmAddress = chainId ? networkMapping[String(chainId)]["TokenFarm"][0] : constants.AddressZero
+    const tokenFarmInterface = new utils.Interface(abi)
+    const tokenFarmContract = new Contract(tokenFarmAddress, tokenFarmInterface)
+
+    const { send: unstakeSend, state: mystate } =
+        useContractFunction(tokenFarmContract, "sellBid", {
+            transactionName: 'Unstake tokens'
+        })
+    const UnStake = () => { unstakeSend(tokenAddress) }
+    return { UnStake, mystate }
+}
+
